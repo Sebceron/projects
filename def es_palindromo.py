e9199dbc5b7c6985e135f40bc5619b069f2a9dc8
Sebceron <151409268+Sebceron@users.noreply.github.com>
2024-05-19 22:27:31 -0500

    Update def es_palindromo.py

--- def es_palindromo.py
+++ def es_palindromo.py
@@ -1,2 +1,16 @@
-def es_palindromo
+def es_palindromo(palabra):
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    return palabra == palabra_invertida
 
+
+palabra_usuario = input(" ingrese una palabra: ")
+
+
+if es_palindromo(palabra_usuario):
+    
+     print("la palabra", palabra_usuario, "es palíndromo")
+
+else:
+
+     print("la palabra", palabra_usuario, "no es palíndromo")
